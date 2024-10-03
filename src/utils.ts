00178d3c613da3b9fd62894e5cf03f93d5c5a90e
notjuliet <notjuliet@riseup.net>
2024-10-03 15:19:45 +0200

    add utils

---
+++ src/utils.ts
@@ -0,0 +1,15 @@
+export const resolveDid = async (did: string) => {
+  const res = await fetch(
+    did.startsWith("did:web") ?
+      `https://${did.split(":")[2]}/.well-known/did.json`
+    : "https://plc.directory/" + did,
+  );
+
+  return res.json().then((doc) => {
+    for (const alias of doc.alsoKnownAs) {
+      if (alias.includes("at://")) {
+        return alias.split("//")[1];
+      }
+    }
+  });
+};
